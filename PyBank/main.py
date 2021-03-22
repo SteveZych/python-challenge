8b71046596d0f5cbc5ce5876e087813280aabcf1
Steven Zych <stevenzych@Stevens-MacBook-Pro.local>
2021-03-22 13:35:22 -0600

    added conditional to implement empty variables and comparison of rows

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,7 +2,7 @@ import os
 import csv
 
 #Path to collect data from the Resources folder
-pollData_csv = os.path.join("..","Resources","budget_data.csv")
+pollData_csv = os.path.join("Resources","budget_data.csv")
 
 #Calculate total number of months in the data set
 months = 0
@@ -22,26 +22,38 @@ greatest_decrease_amount = 0
 with open (pollData_csv, "r") as csvfile:
     
     #split the data on commas
-    csvreader = csv.read(csvfile, delimiter= ",")
+    csvreader = csv.reader(csvfile, delimiter= ",")
+
+    header = next(csvreader)
 
     #loop through the data
-    for row in csvreader:
+    for i, row in enumerate(csvreader):
         months = months + 1
-        netTotal = netTotal + row[1]
-        profit_loss.append(row[1])
-        if row[1] > greatest_increase_amount:
-            greatest_increase_amount = row[1]
-            greatest_decrease_date = row[0]
-        elif row[1] < greatest_decrease_amount:
-            greatest_decrease_amount = row[1]
-            greatest_decrease_date = row[0]
-        else:
-            pass 
-
-#loop through profit and loss and calculate change, append to change list
-for value in profit_loss:
-    difference = value + ()
-    
-
+        #netTotal = netTotal + int(row[1])
+        profit_loss.append(int(row[1]))
+        if i > 0:
+            change_row = int(row[1]) - int(previous_row[1])
+            change.append(change_row)
+            if  change_row > greatest_increase_amount:
+                greatest_increase_amount = change_row
+                greatest_increase_date = row[0]
+            if change_row < greatest_decrease_amount:
+                greatest_decrease_amount = change_row
+                greatest_decrease_date = row[0]
+        previous_row = row
+        
+sum_profit_loss = sum(profit_loss)
+average_change = (sum(change) / len(change)      
 
 #print analysis to terminal and and export a test file with the resutls.
+
+output = (f'''
+Financial Analysis
+----------------------------
+Total Months: {months}
+Total: ${sum_profit_loss}
+Average  Change: $-2315.12 ${average_change}
+Greatest Increase in Profits: {greatest_increase_date} (${greatest_increase_amount})
+Greatest Decrease in Profits: Sep-2013 {greatest_decrease_date} (${greatest_decrease_amount})
+''')
+print(output)
